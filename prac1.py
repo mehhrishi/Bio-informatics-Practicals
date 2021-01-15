305485dfd615ba5537550d2f0238e6d7fcaffa68
mehhrishi <65587663+mehhrishi@users.noreply.github.com>
2021-01-15 22:22:51 +0530

    Add files via upload

---
+++ prac1.py
@@ -0,0 +1,42 @@
+#Practical 1
+
+#Write a Python/Java code to perform Pairwise alignment.
+#Take 2 sequences from user and calculate the score.
+
+
+#code:
+
+se1=input("Enter the first sequence::")
+se2=input("Enter the second sequence::")
+seq1=list(se1)
+seq2=list(se2)
+score=[]
+
+def Pairwise_alignment(a,b):
+    gap(a,b)
+    print(a)
+    print(b)
+    value=0
+    length=len(a)
+    for i in range(0,length):
+        if(a[i]==b[i]):
+            
+            score.append('1')
+            value=value+1
+        else:
+            
+            score.append('0')
+    print(score)
+    print(value)
+def gap(a,b):
+            if(len(a)==len(b)):
+               print()
+            else:
+                k=int(input("enter the position to insert::"))
+                if(len(a)<len(b)):
+                    a.insert(k,'_')
+                else:
+                    b.insert(k,'_')
+            return(a,b)
+
+Pairwise_alignment(seq1,seq2)
